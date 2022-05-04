f9cf5b408a4790fb785c481a0569269b0ab8425a
Tran Thieu <thieuhaitam@gmail.com>
2022-05-04 11:35:14 +0700

    added mobile tab navigation

---
+++ mobile-tab-navigation/app.js
@@ -0,0 +1,21 @@
+const img__urls = [
+  'https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+  'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+  'https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+  'https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80',
+]
+const phoneImage = document.querySelector('.phone__img')
+const navItems = document.querySelectorAll('.nav__item')
+function renderPage(pageNumber) {
+  phoneImage.style.backgroundImage = `url(${img__urls[pageNumber]})`
+  navItems.forEach(item => {
+    item.classList.remove('active')
+  })
+  navItems[pageNumber].classList.add('active')
+}
+renderPage(0)
+navItems.forEach((navItem, index) => {
+  navItem.addEventListener('click', () => {
+    renderPage(index)
+  })
+})
